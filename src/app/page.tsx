9f5caab3b679e100561a90fa1afc8416618c6f17
Giyu2 <derdene719@gmail.com>
2025-01-27 01:39:12 -0800

    hoho

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="bg-red-600">Click me</Button>
+    </div>
+  )
+}
+
+
+
